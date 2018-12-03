9165c0b6dba64b85b9af487da7d78842bd5b2924
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-03 15:29:49 +0530

    completed erlang pattern matching

--- erlang/pattern_matching.erl
+++ erlang/pattern_matching.erl
@@ -1,5 +1,23 @@
 #!/usr/bin/env escript
 
+-record (name, {first = "", last = "", salutation = ""}).
+
+capitalize(#name {first = FName, last = LName}) ->
+	string:to_upper(FName) ++ " " ++ string:to_upper(LName).
+
+mid(A1, A2) -> (A1 + A2) / 2.
+
+midpoint(P, P) -> P;
+midpoint(P1, {0, 0}) -> P1;
+midpoint({0, 0}, P2) -> P2;
+midpoint({X1, Y1}, {X2, Y2}) -> {mid(X1, X2), mid(Y1, Y2)}.
+
 main([]) ->
-  io:format("~p ~p!~n", [hello, 3]).
+  Name = #name {first = "Dhaval", last = "Dalal", salutation = "Mr."},
+  io:format("~p~n", [Name]),
+  io:format("~p~n", [capitalize(Name)]),
+  io:format("~p~n", [midpoint({2,3}, {4,5})]),
+  io:format("~p~n", [midpoint({0,0}, {4,5})]),
+  io:format("~p~n", [midpoint({2,3}, {0,0})]),
+  io:format("~p~n", [midpoint({2,3}, {2,3})]).
   
