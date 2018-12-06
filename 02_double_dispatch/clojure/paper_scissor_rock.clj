b3dcc2f43343393630650d17a290b73ba83f999f
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-06 09:30:25 +0530

    build equivalence with haskell solution

--- 02_double_dispatch/clojure/paper_scissor_rock.clj
+++ 02_double_dispatch/clojure/paper_scissor_rock.clj
@@ -1,24 +1,27 @@
+; We’ll use record to represent the three choices.
+(defrecord Paper [])
+(defrecord Scissor [])
+(defrecord Rock [])
+
 ; Multi-methods can be used to solve this problem
 ; Here is our dispatch function
 (defmulti beats
-  (fn [e1 e2] [e1 e2]))
+  (fn [e1 e2] [(class e1) (class e2)]))
 
-; We’ll use keys :rock  :paper and :scissor
-; to represent the three choices.
-(defmethod beats [:paper :scissor] [e1 e2] "paper loses")
-(defmethod beats [:paper :rock] [e1 e2] "paper wins")
-(defmethod beats [:scissor :paper] [e1 e2] "scissor wins")
-(defmethod beats [:scissor :rock] [e1 e2] "scissor loses")
-(defmethod beats [:rock :paper] [e1 e2] "rock loses")
-(defmethod beats [:rock :scissor] [e1 e2] "rock wins")
-(defmethod beats :default [e1 e2] "draw")
+(defmethod beats [Paper   Scissor] [e1 e2] "paper loses")
+(defmethod beats [Paper   Rock]    [e1 e2] "paper wins")
+(defmethod beats [Scissor Paper]   [e1 e2] "scissor wins")
+(defmethod beats [Scissor Rock]    [e1 e2] "scissor loses")
+(defmethod beats [Rock    Paper]   [e1 e2] "rock loses")
+(defmethod beats [Rock    Scissor] [e1 e2] "rock wins")
+(defmethod beats :default          [e1 e2] "draw")
 
-(println "paper beats paper = "     (beats :paper :paper))
-(println "paper beats scissor = "   (beats :paper :scissor))
-(println "paper beats rock = "      (beats :paper :rock))
-(println "scissor beats paper = "   (beats :scissor :paper))
-(println "scissor beats scissor = " (beats :scissor :scissor))
-(println "scissor beats rock = "    (beats :scissor :rock))
-(println "rock beats paper = "      (beats :rock :paper))
-(println "rock beats scissor = "    (beats :rock :scissor))
-(println "rock beats rock = "       (beats :rock :rock))
+(println "paper beats paper = "     (beats (Paper.)   (Paper.)))
+(println "paper beats scissor = "   (beats (Paper.)   (Scissor.)))
+(println "paper beats rock = "      (beats (Paper.)   (Rock.)))
+(println "scissor beats paper = "   (beats (Scissor.) (Paper.)))
+(println "scissor beats scissor = " (beats (Scissor.) (Scissor.)))
+(println "scissor beats rock = "    (beats (Scissor.) (Rock.)))
+(println "rock beats paper = "      (beats (Rock.)    (Paper.)))
+(println "rock beats scissor = "    (beats (Rock.)    (Scissor.)))
+(println "rock beats rock = "       (beats (Rock.)    (Rock.)))
