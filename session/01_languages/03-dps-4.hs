ee0bcb4c9df673afc6b201a3dc82b677f318542c
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-12-17 13:04:12 +0530

    updated session example to distance, removed mid-point

--- session/01_languages/03-dps-4.hs
+++ session/01_languages/03-dps-4.hs
@@ -1,18 +1,15 @@
-import Data.Char (toUpper)
-
-mid_point :: (Double, Double) -> (Double, Double) -> (Double, Double)
-mid_point (0,0) p2 = p2
-mid_point p1 (0,0) = p1
-mid_point p1@(x1,y1) p2@(x2,y2)
-  | p1 == p2 = p2
-  | otherwise = (mid x1 x2, mid y1 y2)
-      where
-        mid a1 a2 = (a1 + a2) / 2
+distance :: (Double, Double) -> (Double, Double) -> Double
+distance (0,0) (0,0) = 0
+distance p1@(x1,y1) p2@(x2,y2)
+  | p1 == (0,0) || p2 == (0,0) = (let (x,y) = if p1 == (0,0) then p2 else p1 in sqrt (square x + square y))
+  | otherwise = sqrt $ square (x2 - x1) + square (y2 - y1)
+ where
+    square n = n ^^ 2
 
 main :: IO ()
 main = do
-  print $ mid_point (2,3) (4,5)
-  print $ mid_point (2,3) (0,0)
-  print $ mid_point (0,0) (4,5)
-  print $ mid_point (0,0) (0,0)
+  print $ distance (0,0) (0,0)
+  print $ distance (3,0) (0,0)
+  print $ distance (0,0) (0,4)
+  print $ distance (3,0) (0,4)
   print "DONE"
