4568ba70a548855d7a5d2afa66c7ea6c7888d28b
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2019-05-29 05:09:13 +0530

    Final clean-up

--- 01_languages/c#/destructuring_and_pattern_matching.cs
+++ 01_languages/c#/destructuring_and_pattern_matching.cs
@@ -28,6 +28,10 @@ class destructuring_and_pattern_matching {
     Console.WriteLine(d);
 
     // Destructuring list
+    // It turns out not only tuples can be deconstructed but any type 
+    // which has Deconstruct instance (or extension) method with matching 
+    // signature. 
+    // Look at IEnumerableExtensions below for Deconstruction of IEnumerables.
     var list = new List<int>{1, 2, 3, 4, 5, 6};
 
     // Destructuring gives us a short-hand way of naming parts of the data-structure.
@@ -50,6 +54,7 @@ class destructuring_and_pattern_matching {
     Console.WriteLine(rest);
 
     // De-structuring a custom type
+    // Look at the Name class below for Deconstruction.
     var name = new Name("Dhaval", "Dalal", "Mr.");
     Console.WriteLine(name);
     var (firstName, lastName) = name;
@@ -62,13 +67,13 @@ class destructuring_and_pattern_matching {
     }
     Console.WriteLine(Capitalize(new Name("Dhaval", "Dalal", "Mr.")));
 
-    // Pattern Matching
+    // Deconstructing and Pattern Matching 
+    // Look at the Point class below for Deconstruction.
     double Distance(Point p1, Point p2) {
       double Pythagorean(double x, double y) => Math.Sqrt(x * x + y * y);
       
       var origin = new Point(0, 0);
-      var points = (p1, p2);
-      switch (points) {
+      switch ((p1, p2)) {
         case ValueTuple<Point, Point> p when p.Item1 == p.Item2:
           return 0;
         case ValueTuple<Point, Point> p when (p.Item1 == origin) || (p.Item2 == origin):
@@ -87,9 +92,6 @@ class destructuring_and_pattern_matching {
   }
 }
 
-// It turns out not only tuples can be deconstructed but any type 
-// which has Deconstruct instance (or extension) method with matching 
-// signature. 
 public static class IEnumerableExtensions {
   public static void Deconstruct<T>(this IEnumerable<T> @this, out T first, out IEnumerable<T> rest) {
     if (@this == null) {
@@ -109,6 +111,27 @@ public static class IEnumerableExtensions {
                                     out T second, out T third, 
                                     out IEnumerable<T> rest) => 
     (first, second, (third, rest)) = @this;
+}
+
+class Name {
+  private readonly string fName;
+  private readonly string lName;
+  private readonly string salutation;
+  
+  public Name(string fName, string lName, string salutation) => 
+    (this.fName, this.lName, this.salutation) = (fName, lName, salutation);
+  
+  public void Deconstruct(out string fName, out string lName) {
+    fName = this.fName;
+    lName = this.lName;
+  }
+  
+  public void Deconstruct(out string fName, out string lName, out string salutation) =>
+    ((fName, lName), salutation) = (this, this.salutation);
+
+  public override string ToString() {
+    return $"Name({fName}, {lName}, {salutation})";
+  }
 }
 
 public class Point : IEquatable<Point> {
@@ -148,23 +171,3 @@ public class Point : IEquatable<Point> {
   public override string ToString() => $"Point({x}, {y})";
 }
 
-class Name {
-  private readonly string fName;
-  private readonly string lName;
-  private readonly string salutation;
-  
-  public Name(string fName, string lName, string salutation) => 
-    (this.fName, this.lName, this.salutation) = (fName, lName, salutation);
-  
-  public void Deconstruct(out string fName, out string lName) {
-    fName = this.fName;
-    lName = this.lName;
-  }
-  
-  public void Deconstruct(out string fName, out string lName, out string salutation) =>
-    ((fName, lName), salutation) = (this, this.salutation);
-
-  public override string ToString() {
-    return $"Name({fName}, {lName}, {salutation})";
-  }
-}
