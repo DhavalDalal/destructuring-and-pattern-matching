8fab4571a0cf134b71f5442e8bd50cec4252db74
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2019-05-31 20:00:49 +0530

    clean-up of c# subsumed visitor

--- 03_visitor/c#/SubsumedVisitor.cs
+++ 03_visitor/c#/SubsumedVisitor.cs
@@ -15,9 +15,6 @@ class Cylinder : Shape3d {
   public Cylinder(double baseRadius, double height) => 
     (this.baseRadius, this.height) = (baseRadius, height);
   
-  public void Deconstruct(out double baseRadius, out double height) => 
-    (baseRadius, height) = (this.baseRadius, this.height);
-    
   private double BaseArea() => Math.PI * Math.Pow(baseRadius, 2);
 
   private double BaseCircumference() => 2 * Math.PI * baseRadius;
@@ -34,15 +31,12 @@ class Sphere : Shape3d {
   
   public Sphere(double radius) => this.radius = radius;
 
-  public void Deconstruct(out double radius) => radius = this.radius;
-  
   public double SurfaceArea() => 4 * Math.PI * Math.Pow(radius, 2);
   
   public double Volume() => 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
 }
 
 class CompositeShape : Shape3d {
-
   private readonly IList<Shape3d> shapes;
   
   public CompositeShape(IList<Shape3d> shapes) => this.shapes = shapes;
@@ -63,36 +57,22 @@ public class SVG : Platform {}
 public class SubsumedVisitor {
   public void Render(Platform p, Shape3d s) {
     switch ((p, s)) {
-      case var vt when vt.Item1 is OpenGL && vt.Item2 is Cylinder: {
-        var shape = vt.Item2;
-        Console.WriteLine($"OpenGL: rendering cylinder {shape.SurfaceArea()}");
+      case var _ when p is OpenGL && s is Cylinder:
+        Console.WriteLine($"OpenGL: rendering cylinder {s.SurfaceArea()}");
         return;
-      }
-      
-      case var vt when vt.Item1 is OpenGL && vt.Item2 is Sphere: {
-        var shape = vt.Item2;
-        Console.WriteLine($"OpenGL: rendering sphere {shape.SurfaceArea()}");
+      case var _ when p is OpenGL && s is Sphere:
+        Console.WriteLine($"OpenGL: rendering sphere {s.SurfaceArea()}");
         return;
-      }
-
-      case var vt when vt.Item1 is SVG && vt.Item2 is Cylinder: {
-        var shape = vt.Item2;
-        Console.WriteLine($"SVG: rendering cylinder {shape.SurfaceArea()}");
+      case var _ when p is SVG && s is Cylinder: 
+        Console.WriteLine($"SVG: rendering cylinder {s.SurfaceArea()}");
         return;
-      }
-      
-      case var vt when vt.Item1 is SVG && vt.Item2 is Sphere: {
-        var shape = vt.Item2;
-        Console.WriteLine($"SVG: rendering sphere {shape.SurfaceArea()}");
+      case var _ when p is SVG && s is Sphere:
+        Console.WriteLine($"SVG: rendering sphere {s.SurfaceArea()}");
         return;
-      }
-
-      case var vt when vt.Item2 is CompositeShape: {
-        var compositeShape = vt.Item2 as CompositeShape;
-        compositeShape.ForEach(shape => Render(vt.Item1, shape));
+      case var _ when s is CompositeShape: 
+        var compositeShape = s as CompositeShape;
+        compositeShape.ForEach(shape => Render(p, shape));
         return;
-      }
-        
       default:
         Console.WriteLine();  
         return;
